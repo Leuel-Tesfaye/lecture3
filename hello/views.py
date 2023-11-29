72cab0d28114e5e9776a3798000352ef92bfa6dc
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-11-29 14:03:46 +0300

    changes made in the notes about django

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+#? creating a function that takes (request) as an argument : and this argument is going to represent
+#? the HTTP request that the user made in order to access our web server
+
+def index(request):
+    return HttpResponse("Hello, World !")
+
